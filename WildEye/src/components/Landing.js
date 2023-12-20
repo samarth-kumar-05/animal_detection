f13a88d39532aafeafaf3277d8c318e96cc2e823
samarth-kumar-05 <samarthkumar051102@gmail.com>
2023-12-20 13:24:28 +0530

    first commit

---
+++ WildEye/src/components/Landing.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Detect from './Detect';
+import react from './logoicons/react.png';
+import tf from './logoicons/tf.png';
+import keras from './logoicons/keras.png';
+import colab from './logoicons/colab.png';
+
+const Landing = () => {
+    return (
+      
+
+        <div className="site-section bg-dark" id="demo-section" data-aos="fade">
+
+          <div style={{ marginTop: '0px', marginBottom: '0px' }}>
+            <Detect/>
+          </div>
+        </div>
+
+ 
+    )
+}
+
+export default Landing;
